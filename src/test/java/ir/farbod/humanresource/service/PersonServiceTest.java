e67367872caa649fc2937ebcd58c874521de65b2
farbod2u <saeedsaf@gmail.com>
2021-07-10 16:49:56 +0430

    1400-04-19 PersonService test -> save()

--- src/test/java/ir/farbod/humanresource/service/PersonServiceTest.java
+++ src/test/java/ir/farbod/humanresource/service/PersonServiceTest.java
@@ -1,42 +1,65 @@
 package ir.farbod.humanresource.service;
 
+import ir.farbod.humanresource.entity.Person;
 import ir.farbod.humanresource.repository.PersonRepository;
 import ir.farbod.humanresource.repository.SchoolGradeRepository;
+import lombok.SneakyThrows;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.verify;
 
+@ExtendWith(MockitoExtension.class)
 class PersonServiceTest {
 
     @Mock
     private PersonRepository personRepository;
     @Mock
     private SchoolGradeRepository schoolGradeRepository;
-
-    private AutoCloseable autoCloseable;
-
     private PersonService underTest;
+
+    //private AutoCloseable autoCloseable;
 
     @BeforeEach
     void setUp() {
-        autoCloseable = MockitoAnnotations.openMocks(this);
+        //autoCloseable = MockitoAnnotations.openMocks(this);
         underTest = new PersonService(personRepository, schoolGradeRepository);
     }
 
     @AfterEach
     void tearDown() throws Exception {
-        autoCloseable.close();
+        //autoCloseable.close();
     }
 
     @Test
-    @Disabled
     void save() {
+        // given
+        Person person = new Person(
+                0L,
+                "Saeed",
+                "Safaeian",
+                "0123456789",
+                null
+        );
+
+        // when
+        underTest.save(person);
+
+        // then
+        ArgumentCaptor<Person> personArgumentCaptor = ArgumentCaptor.forClass(Person.class);
+        verify(personRepository).save(personArgumentCaptor.capture());
+        Person captoredPerson = personArgumentCaptor.getValue();
+
+        assertThat(captoredPerson).isEqualTo(person);
     }
 
     @Test
@@ -52,11 +75,13 @@ class PersonServiceTest {
     @Test
     void getAll() {
         // when
+
         try {
             underTest.getAll();
         } catch (Exception e) {
-            e.printStackTrace();
+            // do nothing
         }
+
 
         // then
         verify(personRepository).findAll();
